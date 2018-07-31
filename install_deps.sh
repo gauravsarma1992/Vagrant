fc38de0c34356aa854ffa05d0238015c52c22d5a
Gaurav <gauravsarma1992@gmail.com>
2018-07-31 12:19:13 +0530

    Added docker installation

--- install_deps.sh
+++ install_deps.sh
@@ -9,3 +9,18 @@ apt-get install build-essential git libxml2 libxml2-dev libxslt1.1 libxslt1-dev 
 wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
 mkdir $HOME/golang
 tar -xvzf go1.9.linux-amd64.tar.gz -C $HOME/golang
+
+# Docker installation
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
